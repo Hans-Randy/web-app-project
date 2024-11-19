d53e617b0b6c47db19dad8e8557c9730f7fbf399
DanielPham225 <tpham236@my.centennialcollege.ca>
2024-11-19 08:39:52 -0500

    second commit

--- ecommercialweb/src/App.js
+++ ecommercialweb/src/App.js
@@ -1,13 +1,23 @@
-import Navbar from './UI/heading';
-import Modal from './UI/modal';
-import Category from './UI/category';
+import Navbar from "./UI/heading";
+import Content from "./UI/content";
+import Category from "./UI/category";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Category/>
-      {/* <Modal/> */}
+      <Navbar />
+      <div className="container">
+        <div className="grid grid_full-width">
+          <div className="grid_row">
+            <div className="grid_collumn-2">
+              <Category />
+            </div>
+            <div className="grid_collumn-10">
+              <Content />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ ecommercialweb/src/UI/content.jsx
@@ -0,0 +1,160 @@
+import "../layout/css/layout.css";
+import "../layout/css/content.css";
+import "../layout/css/responsive.css";
+
+function Content() {
+  return (
+    <div className="home-product">
+      {/* product item */}
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/chairproduct2.webp)",
+            }}
+            className="home-product-item_img"
+          />
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/headphoneproduct4.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+          <h4 className="home-product-item_name">Gaming Headphone USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/mouseproduc1.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+          <h4 className="home-product-item_name">Gaming mouse USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/monitorproduct5.avif)",
+            }}
+            className="home-product-item_img"
+          />
+          <h4 className="home-product-item_name">Gaming Monitor</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+      <div className="grid_collumn-2-4">
+        <div className="home-product-item">
+          <div
+            style={{
+              backgroundImage: "url(./img/keyboardproduct3.jpg)",
+            }}
+            className="home-product-item_img"
+          />
+  
+
+          <h4 className="home-product-item_name">Gaming chair USA</h4>
+          <div className="home-product-item_price">500$</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
