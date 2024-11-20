05ba1e9a53131d128112f876a61e10af00ad3333
Hans-Randy <hansrandy.masamba@outlook.com>
2024-11-20 11:14:22 -0500

    Add product model

---
+++ backend/models/Product.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true, unique: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: String, required: true },
+    imageBase64: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
