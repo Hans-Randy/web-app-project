b9f595db497fdb4d3844f196454776262fb368b1
Hans-Randy <hansrandy.masamba@outlook.com>
2024-12-10 23:28:37 -0500

    Update cors options

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ connectToDB(process.env.MONGODB_CONNECTION_STRING, process.env.BUCKET_NAME);
 
 // List of allowed origins
 const allowedOrigins = [
-  "https://e-commerce-group-1.netlify.app/",
+  "https://e-commerce-group-1.netlify.app",
   "http://localhost:3000", // Include your local development URL
 ];
 
